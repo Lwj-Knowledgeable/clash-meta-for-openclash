aabd44b54bcd7f332f702f208b4c6d61b94e97dc
EkkoG <beijiu572@gmail.com>
2023-03-23 18:04:00 +0800

    Use snapshot golang to build

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 cp feeds.conf.default feeds.conf
-echo "src-link clash $(pwd)/clash" >> ./feeds.conf
-./scripts/feeds update clash
-./scripts/feeds update packages
-make defconfig
-./scripts/feeds install -p clash -f clash
+echo "src-link openwrt_packages $(pwd)/openwrt-packages" >> ./feeds.conf
 
-make package/clash/download V=s
-make package/clash/check V=s
-make package/clash/compile V=s
+mkdir openwrt-packages
+
+git clone --depth=1 https://github.com/openwrt/packages official-packages
+sudo cp -r official-packages/lang/golang openwrt-packages/golang
+rm -rf official-packages
+
+cp clash-meta-for-openclash openwrt-packages/clash-meta-for-openclash -r
+sed -i '/golang-package.mk/ c\include $(TOPDIR)/feeds/openwrt_packages/golang/golang-package.mk' openwrt-packages/clash-meta-for-openclash/Makefile
+./scripts/feeds update openwrt_packages
+make defconfig
+./scripts/feeds install -p openwrt_packages -f clash-meta-for-openclash
+
+make package/clash-meta-for-openclash/download V=s
+make package/clash-meta-for-openclash/check V=s
+make package/clash-meta-for-openclash/compile V=s
